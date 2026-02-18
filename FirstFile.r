af72ea71deec1573883da47a2cb312353fa4cbd8
ahtarv <atharvkpatil@gmail.com>
2026-02-18 21:23:57 +0530

    first two visualizations done

--- FirstFile.r
+++ FirstFile.r
@@ -3,3 +3,4 @@ print(typeof(model1))
 #well all standard practices of notations apply but along with that cant start with a number and along with starting with '.' is allowed, ex .pairs <- 100
 #also a new datatype called Raw, adn character is both a single alphabet and multiple too
 #also by default number is double
+#order of operations is just board mass, also might just do a network traffic analysis cause i know for a fact i will learn with a project
--- NetworkTrafficAnalysis.r
+++ NetworkTrafficAnalysis.r
@@ -1,3 +1,6 @@
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+#this helps in install.packages thing
+
 #my first R project. Network traffic analyssis Visualization
 
 #my first dummy data generation
@@ -30,3 +33,33 @@ print(head(traffic_data))
 
 #now time for visualization yeahhh
 
+install.packages("ggplot2")
+library(ggplot2)
+
+options(repr.plot.width=10, repr.plot.height=6)
+
+ggplot(traffic_data, aes(x = timestamp, y = bytes_transferred)) +
+  geom_line() +
+  labs(title = "Network Traffic Over Time",
+       x = "Timestamp",
+       y = "Bytes Transferred")
+
+
+#now something called top talkers analysis, basically which ip address generates most traffic
+
+top_talkers <- aggregate(bytes_transferred ~ source_ip, data = traffic_data, FUN = sum) #dk what this fun is, and why ~ is used plus there is an aggregate function 
+top_talkers <- top_talkers[order(top_talkers$bytes_transferred, decreasing = TRUE), ] #and this is decreasing sure, but what top_taler$bytes_transferred
+top_talkers <- head(top_talkers, 10) #these are the top 10 talkers for visualization
+
+ggplot(top_talkers, aes(x = reorder(source_ip, bytes_transferred), y = bytes_transferred, fill = bytes_transferred)) + #this is like basic x and y +
+  geom_bar(stat = "identity", color = "black") + #this is for the geometric bar +
+  labs(title = "Top 10 Source IP Addresses by Traffic",
+       x = "Source IP",
+       y = "Bytes Transferred") +
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45, hjust  =1), #no idea what hijust means
+        plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
+        axis.title.x = element_text(size=12, face="bold"),
+        axis.title.y = element_text(size=12, face="bold"),
+        legend.position = "none")+#these are all just like specifications for dispaly
+    geom_text(aes(label = scales::comma(bytes_transferred)), vjust = -0.3, size = 3.5) #dk what vjust is
