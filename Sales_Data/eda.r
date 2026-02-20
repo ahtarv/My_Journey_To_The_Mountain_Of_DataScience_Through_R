5768bff259ac6814dc5056093c460875a29fa840
ahtarv <atharvkpatil@gmail.com>
2026-02-20 18:26:38 +0530

    yeah it works,

--- Sales_Data/eda.r
+++ Sales_Data/eda.r
@@ -9,8 +9,10 @@ sales_data$date <- as.Date(sales_data$date) # so for this i got that i got that 
 str(sales_data)
 summary(sales_data) # this is the summary
 
-ggplot(sales_data, aes(x = date, y = revenue) + geom_line(alpha = 0.6, color = "steelblue") + labs(title = "Daily Revenue Over Time", x = "Date", y = "Revenue")) +
-    theme_minimal() # so all this is same as before, just dk what alpha is and why we do theme_minial()?
+ggplot(sales_data, aes(x = date, y = revenue)) +
+    geom_line(alpha = 0.6, color = "steelblue") +
+    labs(title = "Daily Revenue Over Time", x = "Date", y = "Revenue") +
+    theme_minimal() # so all this is same as before, just dk what alpha is and why we do theme_minimal()?
 
 monthly_revenue <- sales_data %>%
     mutate(month = floor_date(date, "month")) %>%
@@ -24,7 +26,9 @@ ggplot(monthly_revenue, aes(x = month, y = total_revenue)) +
     theme_minimal()
 # so now this is the same as sales data thing, just i replace date with month and revenue with total revenue
 
-ggplot(sales_data, aes(x = category, y = revenue, fill = category) + geom_boxplot(alpha = 0.7) + labs(title = "Revenue Distribution by Category", x = "Category", y = "Revenue")) +
+ggplot(sales_data, aes(x = category, y = revenue, fill = category)) +
+    geom_boxplot(alpha = 0.7) +
+    labs(title = "Revenue Distribution by Category", x = "Category", y = "Revenue") +
     theme_minimal() +
     theme(legend.position = "none") # this is category wise, same syntax as that of date wise and month wise
 
@@ -36,4 +40,4 @@ sales_data %>% # this symbol once again, first do this then that
         total_revenue = sum(revenue)
     ) # reminds me of nested queries ngl, like first fetch this and this then that, and just dislay it
 
-ggsave("outputs/figure/monthly_revenue_trend.png", width = 8, height = 5) # this is the plot save funcn as photo. i should play with the widtha nd the height more could be interesting.
+ggsave("outputs/figures/monthly_revenue_trend.png", width = 8, height = 5) # this is the plot save funcn as photo. i should play with the width and the height more could be interesting.
