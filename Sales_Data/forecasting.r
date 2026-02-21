0958e7f1b8b920aedbb92ecc03e99099a436fc9e
ahtarv <atharvkpatil@gmail.com>
2026-02-21 12:50:02 +0530

    final things added with proper forecasting

--- Sales_Data/forecasting.r
+++ Sales_Data/forecasting.r
@@ -1,4 +1,4 @@
-install.packages("forecast") # downnloading forecast library
+# install.packages("forecast") # already installed, commenting out to avoid CRAN mirror error in Rscript
 
 
 library(dplyr)
@@ -17,4 +17,29 @@ monthly_revenue <- sales_data %>%
     summarise(total_revenue = sum(revenue)) %>%
     arrange(month) # same before, no change as far as i can see
 
-re
+revenue_ts <- ts(
+    monthly_revenue$total_revenue,
+    start = c(year(min(monthly_revenue$month)), month(min(monthly_revenue$month))),
+    frequency = 12
+) # this is the monthly data with yearly seasonality.
+
+
+naive_model <- naive(revenue_ts, h = 6) # new territory, no idea what all this
+arima_model <- auto.arima(revenue_ts, seasonal = TRUE)
+arima_forecast <- forecast(arima_model, h = 6) # finally forecasting territory, though i dont any of this ngl
+pdf("outputs/figures/revenue_forecast.pdf", width = 9, height = 6)
+
+plot(
+    arima_forecast,
+    main = "6-Month Revenue Forecast (Seasonal ARIMA)", # aye last time we used lab, but this time we are directly using lab in this
+    xlab = "Time",
+    ylab = "Revenue"
+)
+
+lines(naive_model$mean, col = "red", lty = 2)
+legend("topleft", legend = c("Seasonal ARIMA Forecast", "Naive Forecast"), col = c("blue", "red"), lty = c(1, 2), bty = "n") # no idea what this is this is legend sure but of what
+
+dev.off()
+
+checkresiduals(arima_model)
+print(revenue_ts)
